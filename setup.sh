a1d800a319dc908837924636df645718f6ca0c34
CuzzImBatman <54909724+CuzzImBatman@users.noreply.github.com>
2024-11-17 03:23:46 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,3 @@ pip uninstall torch
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
 # crunch setup --notebook --size large broad-1 test --token 2BwKdHlYLLb3FKFYqvyXG9bgZMIonFH52Iu6XQDkIHIb7SvUXWdn6ipSgypRNrqb
 
-cd ./mclSTExp/
