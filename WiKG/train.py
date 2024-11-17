cd735f8c4462bc5ff4aed7f89dbb4c9bdd9649c5
CuzzImBatman <54909724+CuzzImBatman@users.noreply.github.com>
2024-11-18 03:22:14 +0700

    Update train.py

--- WiKG/train.py
+++ WiKG/train.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import torch.backends.cudnn as cudnn
-from dataset import DATA_BRAIN,Dummy
+from dataset import DATA_BRAIN,Dummy,PreprocessedDataset
 from pathlib import Path
 from lr_scheduler import LR_Scheduler
 from torch.utils.data import Sampler
@@ -17,9 +17,9 @@ import random
 from tqdm import tqdm
 from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, cohen_kappa_score, confusion_matrix
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-
+from model import ImageEncoder
 import pandas as pd
-
+# torch.multiprocessing.set_start_method('spawn')
 class CustomBatchSampler(Sampler):
     def __init__(self, dataset, shuffle=True):
         self.dataset = dataset
@@ -94,54 +94,44 @@ def val_one_epoch(model, val_loader, device, data_type='val'):
 
     return preds.cpu(), labels.cpu()
 
+def preprocess_dataset(original_dataset, encoder, device):
+    """
+    Preprocess the images using the encoder and return the features and labels.
+    """
+    encoder.eval()  # Set encoder to evaluation mode
+    features = []
+    exps = []
 
-def cal_metrics(logits, labels, num_classes):       # logits:[batch_size, num_classes]   labels:[batch_size, ]
-    # accuracy
-    predicted_classes = torch.argmax(logits, dim=1)
-    accuracy = accuracy_score(labels.numpy(), predicted_classes.numpy())
+    dataloader = DataLoader(original_dataset, batch_size=1024, shuffle=False)
+    print(len(dataloader))
+    i=0
+    with torch.no_grad():
+        for images, exp in dataloader:
+            images = images.to(device)
+            
+            features_batch = encoder(images)
+            features.append(features_batch.cpu())
+            exps.extend(exp)
+            i=i+1
+            print(i)
+           
 
-    # macro-average area under the cureve (AUC) scores
-    probs = F.softmax(logits, dim=1)
-    if num_classes > 2:
-        auc = roc_auc_score(y_true=labels.numpy(), y_score=probs.numpy(), average='macro', multi_class='ovr')
-    else:
-        auc = roc_auc_score(y_true=labels.numpy(), y_score=probs[:,1].numpy())
-
-    # weighted f1-score
-    f1 = f1_score(labels.numpy(), predicted_classes.numpy(), average='weighted')
-
-    # quadratic weighted Kappa
-    kappa = cohen_kappa_score(labels.numpy(), predicted_classes.numpy(), weights='quadratic')
-
-    # macro specificity 
-    specificity_list = []
-    for class_idx in range(num_classes):
-        true_positive = np.sum((labels.numpy() == class_idx) & (predicted_classes.numpy() == class_idx))
-        true_negative = np.sum((labels.numpy() != class_idx) & (predicted_classes.numpy() != class_idx))
-        false_positive = np.sum((labels.numpy() != class_idx) & (predicted_classes.numpy() == class_idx))
-        false_negative = np.sum((labels.numpy() == class_idx) & (predicted_classes.numpy() != class_idx))
-
-        specificity = true_negative / (true_negative + false_positive)
-        specificity_list.append(specificity)
-
-    macro_specificity = np.mean(specificity_list)
-
-    # confusion matrix
-    confusion_mat = confusion_matrix(labels.numpy(), predicted_classes.numpy())
-
-    return accuracy, auc, f1, kappa, macro_specificity, confusion_mat
+    # Concatenate features along the batch dimension
+    features = torch.cat(features, dim=0)
+    exps=np.stack(exps)
+    return features, exps
 
 
 
 def parse():
     parser = argparse.ArgumentParser('Training for WiKG')
-    parser.add_argument('--epochs', type=int, default=200)
-    parser.add_argument('--batch_size', type=int, default=1024, help='patch_size')
+    parser.add_argument('--epochs', type=int, default=600)
+    parser.add_argument('--batch_size', type=int, default=4096, help='patch_size')
 
     parser.add_argument('--embed_dim', type=int, default=1024, help="The dimension of instance-level representations")
     parser.add_argument('--patch_size', type=int, default=112, help='patch_size')
     parser.add_argument('--utils', type=str, default=None, help='utils path')
-    parser.add_argument('--device', type=str, default='cuda:0', help='device to use for training / testing')
+    parser.add_argument('--device', type=str, default='cuda:1', help='device to use for training / testing')
     parser.add_argument('--n_workers', type=int, default=0)
     parser.add_argument('--seed', default=2023, type=int)  # 3407, 1234, 2023
     parser.add_argument('--n_classes', type=int, default=460)
@@ -161,19 +151,19 @@ def save_checkpoint(epoch, model, optimizer,scheduler, args, filename="checkpoin
         'scheduler': scheduler.state_dict(),
         'args': args
     }
-    dir=f"{args.path_save}/model_result/{args.patch_size}"
+    dir=f"{args.save_dir}/model_result/{args.patch_size}"
     os.makedirs(dir, exist_ok=True)
     torch.save(checkpoint, f"{dir}/{filename}")
     print(f"Checkpoint saved at epoch {epoch}")
 
 def load_checkpoint(epoch, model, optimizer,scheduler,args):
     filename=f"checkpoint_epoch_{epoch}.pth.tar"
-    dir=f"{args.path_save}/model_result/{args.patch_size}"
+    dir=f"{args.save_dir}model_result/{args.patch_size}"
     checkpoint = torch.load(f"{dir}/{filename}")
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    epoch = checkpoint['epoch']
-    args = checkpoint['args']
+    # epoch = checkpoint['epoch']
+    # args = checkpoint['args']
     # scheduler.load_state_dict(checkpoint['scheduler'])
     print(f"Checkpoint loaded from epoch {epoch}")
     return epoch + 1, args,model,scheduler,optimizer
@@ -188,36 +178,52 @@ def main(args):
     cudnn.benchmark = True
 
     utils_dir = args.utils
-
-    train_dataset = DATA_BRAIN(train=True,r=int(args.patch_size/2), device=args.device)
-        
+    encoder= ImageEncoder().to(args.device)
+    # train_dataset = DATA_BRAIN(train=True,r=int(args.patch_size/2), device=args.device)
+    # features, exps = preprocess_dataset(train_dataset, encoder, args.device)
+    # features_np = features.numpy()
+    # np.savez('preprocessed_train.npz', features=features_np, exps=exps)
+    # val_set = DATA_BRAIN(train=False,r=int(args.patch_size/2), device=args.device)
+    # features, exps = preprocess_dataset(val_set, encoder, args.device)
+    # features_np = features.numpy()
+    
+    # np.savez('preprocessed_val.npz', features=features_np, exps=exps)
+    data= np.load('./preprocess/preprocessed_train.npz')
+    # print(features_np)
+    features = torch.from_numpy(data['features'])
+    exps=data['exps']
+    preprocessed_train_dataset = PreprocessedDataset(features, exps)
 
     # print(f'Using fold {args.fold}')
-    print(f'train: {len(train_dataset)}')
+    print(f'train: {len(preprocessed_train_dataset)}')
     # print(f'valid: {len(val_set)}')
 
     dummy_dataset= Dummy(train=True)
     batch_sampler = CustomBatchSampler(dummy_dataset, shuffle=True)
-    train_dataLoader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=batch_sampler,num_workers=4,pin_memory=True)    
+    train_dataLoader = DataLoader(preprocessed_train_dataset, batch_size=args.batch_size, shuffle=batch_sampler,num_workers=args.n_workers,pin_memory=True)    
     
     
-    model = WiKG(dim_in=args.embed_dim, dim_hidden=512, topk=6, n_classes=args.n_classes, agg_type='bi-interaction', dropout=0.3, pool='mean').to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.2, weight_decay=1e-5)
+    model = WiKG(dim_in=args.embed_dim, dim_hidden=1024, topk=6, n_classes=args.n_classes, agg_type='bi-interaction', dropout=0.3, pool='mean').to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-5)
     scheduler = LR_Scheduler(optimizer=optimizer
                              ,num_epochs=args.epochs
-                             ,base_lr=0.2
+                             ,base_lr=0.001
                              ,iter_per_epoch = len(train_dataLoader)
-                             ,warmup_epochs= 20
-                            ,warmup_lr= 0.1
-                            ,final_lr= 0.015
+                             ,warmup_epochs= 10
+                            ,warmup_lr= 0.0003
+                            ,final_lr= 0.00001
                             ,constant_predictor_lr=False
 )
     
-    
+    data= np.load('./preprocess/preprocessed_val.npz')
+    features = torch.from_numpy(data['features'])
+    exps=data['exps']
+    preprocessed_val_dataset = PreprocessedDataset(features, exps)
+    val_loader = DataLoader(preprocessed_val_dataset, batch_size=4096, num_workers=args.n_workers, shuffle=False)
     output_dir = args.save_dir
     
-    val_set = DATA_BRAIN(train=False,r=int(args.patch_size/2), device=args.device)
-    val_loader = DataLoader(val_set, batch_size=2048, num_workers=4, shuffle=False)
+    # val_set = DATA_BRAIN(train=False,r=int(args.patch_size/2), device=args.device)
+    # val_loader = DataLoader(val_set, batch_size=1024, num_workers=args.n_workers, shuffle=False)
     
     os.makedirs(output_dir, exist_ok=True)
 
@@ -233,8 +239,13 @@ def main(args):
 
     max_val_mse = 0.0
     max_val_mae = 0.0
-    
-    for epoch in range(args.start_epoch, args.epochs):
+    start_epoch= args.start_epoch
+    start_epoch, args, model,scheduler, optimizer = load_checkpoint(463, model, optimizer,scheduler,args)
+    for step in range(start_epoch*len(train_dataLoader)):
+        scheduler.step()
+    print(start_epoch)
+    print(f'start epoch: {start_epoch}, batch size: {args.batch_size}')
+    for epoch in range(start_epoch, args.epochs):
         checkpoint_filename = f"checkpoint_best_epoch_{epoch}.pth.tar"
         train_logits = train_one_epoch(model=model, train_loader=train_dataLoader, optimizer=optimizer,scheduler=scheduler, device=device, epoch=epoch + 1)
         if (epoch+1)%2 ==0: 
@@ -255,6 +266,7 @@ def main(args):
                 csv_writer = csv.writer(csvfile)
                 csv_writer.writerow([epoch+1, mse,mae])
         if (epoch+1)%4==0:
+            checkpoint_filename = f"checkpoint_epoch_{epoch}.pth.tar"
             save_checkpoint(epoch, model, optimizer,scheduler, args, filename=checkpoint_filename)
 
         
@@ -262,4 +274,5 @@ def main(args):
 
 if __name__ == '__main__':
     opt = parse()
+    # torch.multiprocessing.set_start_method('spawn')
     main(opt)
