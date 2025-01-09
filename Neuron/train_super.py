5dd59ca122d59e3dda1013ba6afee37ea23ef89d
CuzzImBatman <54909724+CuzzImBatman@users.noreply.github.com>
2025-01-10 02:04:22 +0700

    modified:   Neuron/train_super.py

--- Neuron/train_super.py
+++ Neuron/train_super.py
@@ -221,7 +221,7 @@ def main(args):
     if args.threshold == True:
         train_model= GATModel_thres
     else:
-        train_model= GATModel_SAT
+        train_model= GATModel_3
     if args.train_encoder==True:
         train_model= Encoder_GAT
         args.input_dim= 1024
